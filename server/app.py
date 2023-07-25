cbe2564fa809c5396076b8c8672a2f0c129e23c8
tgartland89 <thomas.gartland@gmail.com>
2023-07-25 16:52:45 -0600

    added login and logout html forms- updated app.py with routes on back end

--- server/app.py
+++ server/app.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify, request, make_response, render_template, session
+from flask import Flask, jsonify, request, make_response, render_template, session, redirect, url_for
 from flask_restful import Resource, fields, marshal 
 from models import User, Teacher, Substitute, SiteAdmin, Course, Review
 from config import app, db, api
 from flask_cors import CORS
 import bcrypt
 
-
 CORS(app, supports_credentials=True)
 app.template_folder = "templates"
+app.secret_key = os.environ.get('SECRET_KEY', 'default-secret-key')
+bcrypt = bcrypt.Bcrypt(app)
 
 user_fields = {
     'id': fields.Integer,
@@ -255,6 +256,107 @@ def substitute_form():
         db.session.commit()
         return make_response({'message': 'Substitute added successfully'}, 201)
     return render_template('substitute_form.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    try:
+        data = request.get_json()
+
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+        if password != confirm_password:
+            return make_response({'errors': ['Password and Confirm Password do not match']}, 400)
+
+        user = User(
+            email=data['email'],
+            password=bcrypt.generate_password_hash(password).decode('utf-8'),
+            role='Teacher',
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        user_id = user.id
+
+        teacher = Teacher(
+            user_id=user.id,
+            name=data['name'],
+            email=data['email'],
+            phone=data['phone'],
+            school=data['school'],
+            location=data['location'],
+            grade_or_course=data['grade_or_course'],
+        )
+        db.session.add(teacher)
+        db.session.commit()
+
+        return make_response({'message': 'New teacher created successfully'}, 201)
+    except Exception as e:
+        print(e)
+        return make_response({'errors': ['Validation errors']}, 400)
+
+@app.route('/substitute-signup', methods=['POST'])
+def substitute_signup():
+    try:
+        data = request.get_json()
+
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+        if password != confirm_password:
+            return make_response({'errors': ['Password and Confirm Password do not match']}, 400)
+
+        user = User(
+            email=data['email'],
+            password=bcrypt.generate_password_hash(password).decode('utf-8'),
+            role='Substitute',
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        user_id = user.id
+
+        substitute = Substitute(
+            user_id=user_id,
+            name=data['name'],
+            email=data['email'],
+            phone=data['phone'],
+            location=data['location'],
+            grade_or_course=data['grade_or_course'],
+            verification_id=data['verification_id'],
+            qualifications=data['qualifications'],
+        )
+
+        db.session.add(substitute)
+        db.session.commit()
+
+        return make_response({'message': 'New substitute created successfully'}, 201)
+    except Exception as e:
+        print(e)
+        return make_response({'errors': ['Validation errors']}, 400)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    if not email or not password:
+        return make_response({'errors': ['Email and password are required fields']}, 400)
+
+    user = Teacher.query.filter_by(email=email).first() or Substitute.query.filter_by(email=email).first()
+
+    if user and bcrypt.check_password_hash(user.password_hash, password):
+        session['user_id'] = user.id
+        return make_response({'message': 'Login successful'}, 201)
+    else:
+        return make_response({'errors': ['Invalid credentials']}, 401)
+
+@app.route('/logout', methods=['DELETE'])
+def logout():
+    if 'user_id' in session:
+        session.pop('user_id', None)
+        return make_response({'message': 'Logged out successfully'}, 204)
+    else:
+        return make_response({'errors': ['User not logged in']}, 401)
 
 if __name__ == '__main__':
     app.run(port=5555)
