78e39d13f602de914d1c6a1c2470557c7f9cb3d3
tgartland89 <thomas.gartland@gmail.com>
2023-07-29 15:13:08 -0600

    updated log out- looking good!

--- server/app.py
+++ server/app.py
@@ -15,22 +15,40 @@ bcrypt.init_app(app)
 
 @app.route('/')
 def home():
-    home_page_content = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>SubSpot - Home</title>
-    </head>
-    <body>
-        <h1>Welcome to SubSpot!</h1>
-        <p>Find substitutes quickly for your teaching needs.</p>
-        <a href="/login">Log In</a> or <a href="/signup">Sign Up</a>
-    </body>
-    </html>
-    """
+    if 'user_id' in session:
+        home_page_content = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>SubSpot - Home</title>
+        </head>
+        <body>
+            <h1>Welcome to SubSpot!</h1>
+            <p>Find substitutes quickly for your teaching needs.</p>
+            <a href="/logout">Log Out</a>
+        </body>
+        </html>
+        """
+    else:
+        home_page_content = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>SubSpot - Home</title>
+        </head>
+        <body>
+            <h1>Welcome to SubSpot!</h1>
+            <p>Find substitutes quickly for your teaching needs.</p>
+            <a href="/login">Log In</a><br>
+            <a href="/signup">Sign Up</a>
+        </body>
+        </html>
+        """
+
     response = make_response(home_page_content)
     response.headers['Content-Type'] = 'text/html'
     return response
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -45,26 +63,27 @@ def login():
             return redirect(url_for('home'))
 
         return redirect(url_for('login'))
+
     login_page_content = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>SubSpot - Log In</title>
-    </head>
-    <body>
-        <h1>Log In</h1>
-        <form method="post">
-            <label for="email">Email:</label>
-            <input type="email" name="email" required><br>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SubSpot - Log In</title>
+</head>
+<body>
+    <h1>Log In</h1>
+    <form method="post">
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
 
-            <label for="password">Password:</label>
-            <input type="password" name="password" required><br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br>
 
-            <input type="submit" value="Log In">
-        </form>
-    </body>
-    </html>
-    """
+        <input type="submit" value="Log In">
+    </form>
+</body>
+</html>
+"""
     response = make_response(login_page_content)
     response.headers['Content-Type'] = 'text/html'
     return response
@@ -113,105 +132,109 @@ def signup():
             return redirect(url_for('home'))
 
     signup_page_content = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>SubSpot - Sign Up</title>
-    </head>
-    <body>
-        <h1>Sign Up</h1>
-        <form method="post" id="signup-form">
-            <label for="role">Choose Role:</label>
-            <select name="role" id="role" onchange="toggleFormFields()">
-                <option value="Teacher">Teacher</option>
-                <option value="Substitute">Substitute</option>
-            </select>
-            <br><br>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SubSpot - Sign Up</title>
+</head>
+<body>
+    <h1>Sign Up</h1>
+    <form method="post" id="signup-form">
+        <label for="role">Choose Role:</label>
+        <select name="role" id="role" onchange="toggleFormFields()">
+            <option value="Teacher">Teacher</option>
+            <option value="Substitute">Substitute</option>
+        </select>
+        <br><br>
 
-            <label for="name">Name:</label>
-            <input type="text" name="name" required><br>
+        <label for="name">Name:</label>
+        <input type="text" name="name" required><br>
 
-            <label for="email">Email:</label>
-            <input type="email" name="email" required><br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
 
-            <label for="password">Password:</label>
-            <input type="password" name="password" required><br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br>
 
-            <label for="confirm_password">Confirm Password:</label>
-            <input type="password" name="confirm_password" required><br>
+        <label for="confirm_password">Confirm Password:</label>
+        <input type="password" name="confirm_password" required><br>
 
-            <label for="location">Location:</label>
-            <input type="text" name="location" required><br>
+        <label for="location">Location:</label>
+        <input type="text" name="location" required><br>
 
-            <!-- School Name (Teacher) -->
-            <label for="school_name" id="school_name_label" style="display:none">School Name:</label>
-            <input type="text" name="school_name" id="school_name_input" style="display:none"><br>
+        <!-- School Name (Teacher) -->
+        <label for="school_name" id="school_name_label" style="display:none">School Name:</label>
+        <input type="text" name="school_name" id="school_name_input" style="display:none"><br>
 
-            <!-- Phone -->
-            <label for="phone">Phone:</label>
-            <input type="text" name="phone" required><br>
+        <!-- Phone -->
+        <label for="phone">Phone:</label>
+        <input type="text" name="phone" required><br>
 
-            <!-- Course Name (Teacher) -->
-            <label for="course_name" id="course_name_label" style="display:none">Course Name:</label>
-            <input type="text" name="course_name" id="course_name_input" style="display:none"><br>
+        <!-- Course Name (Teacher) -->
+        <label for="course_name" id="course_name_label" style="display:none">Course Name:</label>
+        <input type="text" name="course_name" id="course_name_input" style="display:none"><br>
 
-            <!-- Qualifications (Substitute) -->
-            <label for="qualifications" id="qualifications_label" style="display:none">Qualifications:</label>
-            <input type="text" name="qualifications" id="qualifications_input" style="display:none"><br>
+        <!-- Qualifications (Substitute) -->
+        <label for="qualifications" id="qualifications_label" style="display:none">Qualifications:</label>
+        <input type="text" name="qualifications" id="qualifications_input" style="display:none"><br>
 
-            <!-- Verification ID (Substitute) -->
-            <label for="verification_id" id="verification_id_label" style="display:none">Verification ID:</label>
-            <input type="text" name="verification_id" id="verification_id_input" style="display:none"><br>
+        <!-- Verification ID (Substitute) -->
+        <label for="verification_id" id="verification_id_label" style="display:none">Verification ID:</label>
+        <input type="text" name="verification_id" id="verification_id_input" style="display:none"><br>
 
-            <input type="submit" value="Sign Up">
-        </form>
+        <input type="submit" value="Sign Up">
+    </form>
 
-        <script>
-            function toggleFormFields() {
-                var role = document.getElementById('role').value;
+    <script>
+        function toggleFormFields() {
+            var role = document.getElementById('role').value;
 
-                var schoolNameLabel = document.getElementById('school_name_label');
-                var schoolNameInput = document.getElementById('school_name_input');
+            var schoolNameLabel = document.getElementById('school_name_label');
+            var schoolNameInput = document.getElementById('school_name_input');
 
-                var courseNameLabel = document.getElementById('course_name_label');
-                var courseNameInput = document.getElementById('course_name_input');
+            var courseNameLabel = document.getElementById('course_name_label');
+            var courseNameInput = document.getElementById('course_name_input');
 
-                var qualificationsLabel = document.getElementById('qualifications_label');
-                var qualificationsInput = document.getElementById('qualifications_input');
+            var qualificationsLabel = document.getElementById('qualifications_label');
+            var qualificationsInput = document.getElementById('qualifications_input');
 
-                var verificationIdLabel = document.getElementById('verification_id_label');
-                var verificationIdInput = document.getElementById('verification_id_input');
+            var verificationIdLabel = document.getElementById('verification_id_label');
+            var verificationIdInput = document.getElementById('verification_id_input');
 
-                if (role === 'Teacher') {
-                    schoolNameLabel.style.display = 'inline-block';
-                    schoolNameInput.style.display = 'inline-block';
-                    courseNameLabel.style.display = 'inline-block';
-                    courseNameInput.style.display = 'inline-block';
+            if (role === 'Teacher') {
+                schoolNameLabel.style.display = 'inline-block';
+                schoolNameInput.style.display = 'inline-block';
+                courseNameLabel.style.display = 'inline-block';
+                courseNameInput.style.display = 'inline-block';
 
-                    qualificationsLabel.style.display = 'none';
-                    qualificationsInput.style.display = 'none';
-                    verificationIdLabel.style.display = 'none';
-                    verificationIdInput.style.display = 'none';
-                } else if (role === 'Substitute') {
-                    schoolNameLabel.style.display = 'none';
-                    schoolNameInput.style.display = 'none';
-                    courseNameLabel.style.display = 'none';
-                    courseNameInput.style.display = 'none';
+                qualificationsLabel.style.display = 'none';
+                qualificationsInput.style.display = 'none';
+                verificationIdLabel.style.display = 'none';
+                verificationIdInput.style.display = 'none';
+            } else if (role === 'Substitute') {
+                schoolNameLabel.style.display = 'none';
+                schoolNameInput.style.display = 'none';
+                courseNameLabel.style.display = 'none';
+                courseNameInput.style.display = 'none';
 
-                    qualificationsLabel.style.display = 'inline-block';
-                    qualificationsInput.style.display = 'inline-block';
-                    verificationIdLabel.style.display = 'inline-block';
-                    verificationIdInput.style.display = 'inline-block';
-                }
+                qualificationsLabel.style.display = 'inline-block';
+                qualificationsInput.style.display = 'inline-block';
+                verificationIdLabel.style.display = 'inline-block';
+                verificationIdInput.style.display = 'inline-block';
             }
-        </script>
-    </body>
-    </html>
-    """
-
+        }
+    </script>
+</body>
+</html>
+"""
     response = make_response(signup_page_content)
     response.headers['Content-Type'] = 'text/html'
     return response
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(port=5555)
